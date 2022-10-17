9141d8372b4d2bd4c823cd2c2bc8bd540976376f
SaraAzevedoB <benedictosara@gmail.com>
2022-10-17 20:27:42 -0300

    Criando form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
+import { Checkbox } from './components/Checkbox';
+import { TextInput } from './components/TextInput';
+import { Text } from './components/Text';
+import { Heading } from './components/Heading';
+import { Logo } from './Logo';
 import './styles/global.css';
+import { Envelope } from 'phosphor-react';
+import { Lock } from 'phosphor-react';
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-5xl text-violet-500'>Hello World</h1>
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
-        Enviar
-      </button>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
 
-    </>
+      <header className='flex flex-col items-center'>
+        <Logo />
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+
+        <Text size='lg' className='text-gray-400 mt-1'>
+          Faço o Login e comece a usar!
+        </Text>
+      </header >
+      <form className='flex flex-col items-stretch gap-4 w-full max-w-sm mt-10'>
+        <label htmlFor="email" className='flex flex-col gap-3 '>
+          <Text className='font-semibold'>
+            Endereço de e-mail
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input type="email" id='email' placeholder='Digite seu e-mail'/>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className='flex flex-col gap-3 '>
+          <Text className='font-semibold'>
+            Sua senha
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input type="password" id='password' placeholder='********'/>
+          </TextInput.Root>
+        </label>
+      
+
+      <label htmlFor="remember" className='flex items-center gap-2'>
+        <Checkbox />
+        <Text size='sm' className='text-gray-200'>
+        Lembrar de mim por 30 dias
+        </Text>
+       
+      </label>
+      </form>
+    </div>
+
   )
 }
 
